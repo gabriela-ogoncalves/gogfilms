939623c73d6fdb6661e05169e1c043a80a3aa16a
gabriela.goncalves <gabriela.goncalves@gitlab.globalhitss.com.br>
2020-07-29 22:30:38 -0300

    Inclusão de Página Não Encontrada

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
 import CadastroCategoria from './pages/cadastro/Categoria';
-
-// Desafio master blaster na descrição
-const Pagina404 = () => (<div>Página 404!</div>)
+import NotFound from './pages/NotFound';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
       <Route path="/" component={Home} exact/>
-      <Route component={Pagina404} />
+      <Route component={NotFound} />
       
     </Switch>
   </BrowserRouter>,
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import PageDefault from '../../components/PageDefault';
+import Erro from '../../assets/img/404.gif'
+import './style.css'
+
+function NotFound() {
+
+    const Container = styled.div`
+        margin-left: 5%;
+        margin-right: 5%;
+        height: 100%;
+        display: flex;
+        flex: 1;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    `;
+
+    const Button = styled.button`
+        margin: 50px;
+        color: var(--white);
+        border: 1px solid var(--white);
+        box-sizing: border-box;
+        cursor: pointer;
+        padding: 16px 24px;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 16px;
+        outline: none;
+        border-radius: 5px;
+        text-decoration: none;
+        display: inline-block;
+        transition: opacity .3s;
+
+        &:hover,
+        &:focus {
+            opacity: .5;
+        }
+    `;
+
+    return(
+        <PageDefault>
+            <Container>
+                <h1>Página não encontrada!</h1>
+                <a href="/">
+                    <img src={Erro} className="img" alt='Página não encontrada'/>
+                </a>
+                <Button as={Link} to="/">
+                    Voltar para Home
+                </Button>
+            </Container>
+        </PageDefault>
+    );
+}
+
+export default NotFound;
