89c0fb17459eff0cac0ee69b6d7da1e1622c5498
gabriela.goncalves <gabriela.goncalves@gitlab.globalhitss.com.br>
2020-07-31 00:17:53 -0300

    personalizando o rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { FooterBase } from './styles';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import FooterBase from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.github.com/gabriela-ogoncalves">
-        <h1>Gabriela Gonçalves</h1>
-      </a>
+      <Link to="/">
+        <img src={Logo} className="Logo" alt="GogFilms Logo" />
+      </Link>
       <p>
-        Orgulhosamente criado durante a
+        Website feito com ReactJS por
+        {' '}
+        <a href="https://www.linkedin.com/in/gabriela-ogoncalves">
+          <b>Gabriela Gonçalves</b>
+        </a>
+      </p>
+      <p>
+        Acessando o
+        {' '}
+        <a href="https://www.github.com/gabriela-ogoncalves">
+          repositório
+        </a>
+        {' '}
+        você pode verificar como foi feito.
+      </p>
+      <p>
+        Orgulhosamente desenvolvido durante a
         {' '}
         <a href="https://www.alura.com.br/imersao-react/">
           Imersão React da Alura
