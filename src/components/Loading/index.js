a7b1726ff8be1eff37bc2598070e850ac8a879b2
gabriela.goncalves <gabriela.goncalves@gitlab.globalhitss.com.br>
2020-08-05 01:37:57 -0300

    correção loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,19 +4,28 @@ import ReactLoading from 'react-loading';
 
 function Loading() {
   const Container = styled.div`
+    position: fixed;
+    top: 0;
+    height: 100%;
+    z-index: 10;
+    width: 100%;
+    background-color: #000000e9;
     display: flex;
     align-items: center;
-    flex-direction: column;
-    background: var(--black);
-    --bodyPaddingTop: 20px;
-    min-height: calc(60vh);
-    padding-top: 20vh;
-    `;
+    justify-content: center;
+  `;
+
+  const P = styled.p`
+    font-size: 16px;
+    font-weight: normal;
+  `;
 
   return (
     <Container>
-      Carregando...
-      <ReactLoading type="bubbles" color="var(--theme2)" height="100px" width="100px" />
+      <P>
+        Carregando...
+        <ReactLoading type="bubbles" color="var(--theme2)" height="100px" width="100px" />
+      </P>
     </Container>
   );
 }
